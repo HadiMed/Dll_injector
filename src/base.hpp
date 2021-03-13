4cd81fbb549ffa59b2b62c5b8f4c0cc256f5da5c
Hadi Mohammed <57273771+HadiMed@users.noreply.github.com>
2021-03-13 16:33:22 +0100

    Update base.hpp

--- src/base.hpp
+++ src/base.hpp
@@ -1,11 +1,5 @@
 // Retrive base adress of an executable using CreateRemoteThread to bypass ASLR 
 
-#include <iostream>
-#include <stdio.h>
-#include <windows.h>
-#include <tlhelp32.h>
-
-
 DWORD getPID(std::wstring name) // Get pid by name of process 
 {
 	DWORD PID = -1;
@@ -23,70 +17,70 @@ DWORD getPID(std::wstring name) // Get pid by name of process
 			if (binaryPath.find(name) != std::wstring::npos)
 			{
 				PID = entry.th32ProcessID;
-				 
+
 				break;
 			}
 		}
 	}
 
 	CloseHandle(snapshot);
-	return PID; 
+	return PID;
 }
 
 
 
-DWORD get_base(int argc, wchar_t** argv)
+DWORD get_base(std::wstring name)
 {
-	
 
+	
 	DWORD base;
 	HMODULE k32 = GetModuleHandleA("kernel32.dll");
-	
-	
-	LPVOID get_handle = GetProcAddress(k32, "LoadLibraryA");
 
-	DWORD pid = getPID(argv[1]);
+
+	LPVOID get_handle = GetProcAddress(k32, "GetModuleHandleA");
+
+	DWORD pid = getPID(name);
 
 
 
 
 	HANDLE process = OpenProcess( // Open a Handle to process to inject to . 
-		PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE , 
-		FALSE , 
+		PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE,
+		FALSE,
 		pid
 	);
 
 
 
 
-	if (process==INVALID_HANDLE_VALUE) {
+	if (process == INVALID_HANDLE_VALUE) {
 		printf("Some error occured code error  : %d", GetLastError());
-		exit(0); 
+		exit(0);
 
 
 	}
 
-	
+
 	HANDLE thread = CreateRemoteThread( // Create the remote thread
-		process ,
-		NULL , 
-		NULL ,
+		process,
+		NULL,
+		NULL,
 		(LPTHREAD_START_ROUTINE)get_handle,
 		NULL,
 		NULL,
 		NULL
-	
+
 	);
 
 
-	WaitForSingleObject(thread,INFINITE);
+	WaitForSingleObject(thread, INFINITE);
 
-	GetExitCodeThread(thread, &base); 
+	GetExitCodeThread(thread, &base);
 
 
 	CloseHandle(thread);
 
-	printf("BASE ADDRESS = %x", (DWORD)base);
+	
 
-	return base; 
+	return base;
 }
