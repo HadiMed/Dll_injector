5675a90103b6c60faea4a72a60430644f133545b
Hadi Mohammed <57273771+HadiMed@users.noreply.github.com>
2021-03-13 17:03:37 +0100

    Update base.hpp

--- src/base.hpp
+++ src/base.hpp
@@ -29,7 +29,7 @@ DWORD getPID(std::wstring name) // Get pid by name of process
 
 
 
-DWORD get_base(std::wstring name)
+DWORD get_base(std::wstring name) // get base address of a process knowing its name . 
 {
 
 	
