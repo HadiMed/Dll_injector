33ea59abe4f0a0042a3d456341576b6f9187ca85
Mohammed Hadi <57273771+HadiMed@users.noreply.github.com>
2021-06-28 12:10:17 +0100

    Update Inject.cpp

--- src/Inject.cpp
+++ src/Inject.cpp
@@ -10,8 +10,6 @@
 
 int wmain(int argc, wchar_t** argv) {
 
-	
-
 	if (argc != 3) {
 		printf("\nusage : Inject.exe process_name dllfile.dll\n");
 		exit(1);
@@ -21,11 +19,6 @@ int wmain(int argc, wchar_t** argv) {
 
 	DWORD base;
 	// get base adress of kernel32.dll then loadlibraryA 
-
-	
-
-
-
 	DWORD pid = getPID(argv[1]);
 
 
@@ -39,17 +32,11 @@ int wmain(int argc, wchar_t** argv) {
 
 
 	printf("[*] Process base address  at : 0x%x\n", get_base(argv[1]));
-
-
-
 	HANDLE process = OpenProcess( // Open a Handle to process to inject to . 
 		PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE,
 		FALSE,
 		pid
 	);
-
-	
-
 
 	if (process == INVALID_HANDLE_VALUE) {
 		printf("Some error occured code error  : %d\n", GetLastError());
@@ -81,9 +68,7 @@ int wmain(int argc, wchar_t** argv) {
 	);
 
 	HMODULE k32 = GetModuleHandleA("kernel32.dll");
-
 	LPVOID pointr_loadlibrary = GetProcAddress(k32, "LoadLibraryW");
-
 	HANDLE thread = CreateRemoteThread( // Create the remote thread
 		process,
 		NULL,
@@ -97,17 +82,82 @@ int wmain(int argc, wchar_t** argv) {
 
 
 	WaitForSingleObject(thread, INFINITE);
-
 	GetExitCodeThread(thread, &base);
-
-
 	CloseHandle(thread);
 
-	if (base == 0) {
+	if (base == 0) 
+	{
 		printf("[X] Injection didnt work "); 
 		exit(1); 
 	}
 	
 	printf("[*] Dll injected succesfully !  Loaded at  base address  : 0x%x\n", base);
 
+}
+
+DWORD getPID(std::wstring name) // Get pid by name of process 
+{
+	DWORD PID = -1;
+
+	PROCESSENTRY32 entry;
+	entry.dwSize = sizeof(PROCESSENTRY32);
+
+	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+
+	if (Process32First(snapshot, &entry) == TRUE)
+	{
+		while (Process32Next(snapshot, &entry) == TRUE)
+		{
+			std::wstring binaryPath = entry.szExeFile;
+			if (binaryPath.find(name) != std::wstring::npos)
+			{
+				PID = entry.th32ProcessID;
+
+				break;
+			}
+		}
+	}
+
+	CloseHandle(snapshot);
+	return PID;
+}
+
+
+
+DWORD get_base(std::wstring name) // get base address of a process knowing its name . 
+{
+
+	
+	DWORD base;
+	HMODULE k32 = GetModuleHandleA("kernel32.dll");
+	LPVOID get_handle = GetProcAddress(k32, "GetModuleHandleA");
+	DWORD pid = getPID(name);
+	HANDLE process = OpenProcess( // Open a Handle to process to inject to . 
+		PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE,
+		FALSE,
+		pid
+	);
+	if (process == INVALID_HANDLE_VALUE) 
+	{
+		printf("Some error occured code error  : %d", GetLastError());
+		exit(0);
+	}
+
+
+	HANDLE thread = CreateRemoteThread( // Create the remote thread
+		process,
+		NULL,
+		NULL,
+		(LPTHREAD_START_ROUTINE)get_handle,
+		NULL,
+		NULL,
+		NULL
+
+	);
+
+
+	WaitForSingleObject(thread, INFINITE);
+	GetExitCodeThread(thread, &base);
+	CloseHandle(thread);
+	return base;
 }
